2ff62c0e50f0cba008c4ce89d0f69b31cfe7b9e9
rodrichc <rodrichc1@gmail.com>
2024-04-21 16:51:56 -0300

    ejercicio 7

---
+++ Desafios/07.py
@@ -0,0 +1,25 @@
+"""
+7- La secuencia de Collatz de un número entero se construye de la siguiente forma:
+● si el número es par, se lo divide por dos;
+● si es impar, se le multiplica tres y se le suma uno;
+● La sucesión termina al llegar a uno.
+Desarrolle un programa que entregue la secuencia de Collatz de un número entero:
+"""
+while True:
+    try:
+        collatz = input("Ingrese un numero: ")
+        numero = int(collatz)
+        if numero > 1:
+            while numero > 1:
+                if (numero % 2) == 0:
+                    numero = int (numero / 2)
+                    collatz += " " + str(numero) 
+                else:
+                    numero = int((numero * 3) + 1)
+                    collatz += " " + str(numero)
+            print(collatz)
+        else:
+            print("¡Ingrese un numero mayor a 1!")
+        break
+    except IndexError:
+        print("¡Ingrese un numero!")
